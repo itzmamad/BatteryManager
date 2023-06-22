4c6958fa984daad184f4ba8bbc6a535505a9e88e
itzmamad <mahdavifar_12@yahoo.com>
2023-06-22 18:06:40 +0330

    page 1 complete

---
+++ app/src/main/java/com/example/batterymanager/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.batterymanager
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.Timer
+import kotlin.concurrent.timerTask
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Timer().schedule(timerTask {
+            startActivity(Intent(this@SplashActivity , MainActivity::class.java))
+        } , 7000)
+    }
+}
