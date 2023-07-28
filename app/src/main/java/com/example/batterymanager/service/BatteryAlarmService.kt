7061e6e787cafb8a0aa3c13ac68a4cd36252441c
itzmamad <mahdavifar_12@yahoo.com>
2023-07-28 12:41:06 +0330

    update notification with broadcast

--- app/src/main/java/com/example/batterymanager/service/BatteryAlarmService.kt
+++ app/src/main/java/com/example/batterymanager/service/BatteryAlarmService.kt
@@ -1,9 +1,15 @@
 package com.example.batterymanager.service
 
+import android.annotation.SuppressLint
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationChannelCompat
@@ -12,10 +18,15 @@ import com.example.batterymanager.R
 
 class BatteryAlarmService : Service() {
 
+    var manager: NotificationManager? = null
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         createNotificationChannel()
         startNotification()
+
+        registerReceiver(batteryInfoReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+
         return START_STICKY
     }
 
@@ -30,24 +41,55 @@ class BatteryAlarmService : Service() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val serviceChannel =
                 NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_MIN)
-            val manager = getSystemService(NotificationManager::class.java)
-            manager.createNotificationChannel(serviceChannel)
+            manager = getSystemService(NotificationManager::class.java)
+            manager?.createNotificationChannel(serviceChannel)
+        }
+    }
+
+
+    private var batteryInfoReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        @SuppressLint("SetTextI18n")
+        override fun onReceive(context: Context, intent: Intent) {
+            var batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
+
+            var plugState = ""
+            if (intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0) == 0) {
+               // binding.txtPlug.text = "plug-out"
+                plugState = "your phone using battery"
+            } else {
+               // binding.txtPlug.text = " plug-in "
+                plugState = "your phone is charging "
+            }
+
+            updateNotification(batteryLevel ,plugState )
         }
     }
 
     private fun startNotification() {
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("My Title")
-            .setContentText("this is my content")
+            .setContentTitle("Loading...")
+            .setContentText("wait for battery data!")
             .setSmallIcon(R.drawable.health_good)
             .build()
 
 
-        startForeground(1, notification)
+        startForeground(NOTIFICATION_ID, notification)
+    }
+
+
+    private fun updateNotification(batteryLevel: Int, plugState: String) {
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(plugState)
+            .setContentText("Battery charge : $batteryLevel ")
+            .setSmallIcon(R.drawable.health_good)
+            .build()
+
+        manager?.notify(NOTIFICATION_ID, notification)
     }
 
     companion object {
         const val CHANNEL_ID = "BatteryManagerChannel"
         const val CHANNEL_NAME = "BatteryManagerSERVICE"
+        const val NOTIFICATION_ID = 1
     }
 }
